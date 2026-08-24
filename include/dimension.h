efe92a1e5faf0dfa0412829df2c08baa90b7c758
KiSun SONG <codehiker42@gmail.com>
2026-08-24 04:10:03 +0100

    concept of Dimension

---
+++ include/dimension.h
@@ -0,0 +1,27 @@
+#ifndef _PP1_DIMENSION_H_
+#define _PP1_DIMENSION_H_
+
+#include <cstddef>
+#include <initializer_list>
+#include <vector>
+
+namespace pp1 {
+
+class Dimension {
+ public:
+  Dimension() = default;
+  Dimension(std::initializer_list<size_t> elems);
+
+  size_t Size() const;
+
+  size_t NumberOf() const;
+
+  bool operator==(const Dimension& another) const;
+  bool operator!=(const Dimension& another) const;
+
+ private:
+  std::vector<size_t> dim_elems_;
+};
+
+}  // namespace pp1
+#endif
---
+++ src/dimension.cc
@@ -0,0 +1,24 @@
+#include "dimension.h"
+
+#include <numeric>
+
+namespace pp1 {
+
+Dimension::Dimension(std::initializer_list<size_t> elems) : dim_elems_(elems) {}
+
+size_t Dimension::Size() const {
+  return std::accumulate(dim_elems_.begin(), dim_elems_.end(), 1ULL,
+                         std::multiplies<>());
+}
+
+size_t Dimension::NumberOf() const { return dim_elems_.size(); }
+
+bool Dimension::operator==(const Dimension& another) const {
+  return this->dim_elems_ == another.dim_elems_;
+}
+
+bool Dimension::operator!=(const Dimension& another) const {
+  return this->dim_elems_ != another.dim_elems_;
+}
+
+}  // namespace pp1
