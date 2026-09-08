81acf3854b70a5fddc00f368f0ceed2bc3574f3f
KiSun SONG <codehiker42@gmail.com>
2026-09-08 16:48:37 +0100

    Utility functions for MdInitList

--- include/pp1_utility.h
+++ include/pp1_utility.h
@@ -10,6 +10,23 @@ inline constexpr auto Arguments(Args&&... args) {
   return std::forward_as_tuple(args...);
 }
 
+template <class... T>
+inline constexpr auto Arr(T&&... values) {
+  return std::array{std::forward<T>(values)...};
+}
+
+template <typename T, typename U, size_t N>
+  requires std::is_convertible_v<U, T> && (!IsStdArray<U>::value)
+auto MakeMD(const std::array<U, N>& array) {
+  return MDInitList<T, U, U, N>(array);
+}
+
+template <typename T, typename U, size_t N>
+  requires IsStdArray<std::remove_cvref_t<U>>::value
+auto MakeMD(const std::array<U, N>& array) {
+  return typename MDInitListHelper<T, U, U, N>::mdlist_type(array);
+}
+
 }  // namespace pp1
 
 #endif
